4893142b2211eba3f636be0a389d6f6feb89c6b0
Edi0501 <edytanowakowska05@gmail.com>
2024-02-18 12:43:25 +0100

    Add project files.

---
+++ zadanie6New/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace zadanie9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+
+                Console.WriteLine("Wpisz proszę swój wzrost");
+                Console.WriteLine("Wzrost:");
+                int a = 0;
+
+                int.TryParse(Console.ReadLine(), out a);
+
+                {
+                    if (a < 140)
+                    {
+                        Console.WriteLine("Jesteś pokurcz:");
+                    }
+                    else if (a <= 170)
+                    {
+                        Console.WriteLine("Jesteś średniaczek:");
+                    }
+                    else if (a <= 185)
+                    {
+                        Console.WriteLine("Jesteś w sam raz:");
+                    }
+                    else if (a >= 186)
+                    {
+                        Console.WriteLine("Jesteś olbrzymem:");
+                    }
+                    
+                }
+
+
+            }
+        }
+    }
+}
